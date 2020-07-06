88979d787b53a1065f7be48b7ff89199239a0495
Jeeva S <jeevacr7silva@gmail.com>
2020-07-06 10:03:10 +0530

    Updates

---
+++ precache-manifest.f28cf18475b6f387671cd0adc7d1afa6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ab3b476fda72ce3f5430cdcf40eba94",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d835aabeb116d67faebb",
+    "url": "/monsters-rolodex/static/css/main.80e51570.chunk.css"
+  },
+  {
+    "revision": "8a6eda421bc5765c55c2",
+    "url": "/monsters-rolodex/static/js/2.bc65f737.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.bc65f737.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d835aabeb116d67faebb",
+    "url": "/monsters-rolodex/static/js/main.9631ee88.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
